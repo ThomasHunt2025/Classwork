31c3d26f4317310770fae513204d758306d88141
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-05-05 22:07:29 +0000

    List testing 2

---
+++ Lists/TestingLists3.py
@@ -0,0 +1,8 @@
+Study_group = ['Thomas', 'Frankie', 'Benjamin', 'Max'] 
+
+student = input('What is your name? ')
+
+if student in Study_group: 
+    print(f"{student} is in the study group") 
+else:
+    print(f"{student} is not on the list") 
