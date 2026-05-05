dc55f0df1e3322534aaf1bd1e18608821bf293ea
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-05-05 22:31:48 +0000

    Testing lists

--- Lists/TestingLists4.py
+++ Lists/TestingLists4.py
@@ -1,1 +1,11 @@
+study_group = ['Thomas', 'Frankie', 'Benjamin', 'Max'] 
+
+for i in range(3): 
+    name = input('Enter a name: ')
+
+if name in study_group: 
+    print('Those names are in the study group.')
+else:
+    print('Those names are not in the study group.')
+
 
