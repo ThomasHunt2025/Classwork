10cf5b5078e7a335468294c7c691c014952f0ee9
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-05-05 21:58:50 +0000

    List testing

---
+++ Lists/TestingLists1.py
@@ -0,0 +1,8 @@
+favourite_games = ['Minecraft', 'Roblox', 'Fortnite', 'Pokemon', 'Call of Duty']
+
+game = input('What is your favourite game? ')
+
+if game in favourite_games:
+    print('I like that game too!')
+else:
+    print("I don't like that game.")
---
+++ Lists/TestingLists2.py
@@ -0,0 +1,8 @@
+favourite_rugby_players = ['Richie McCaw', 'Dan Carter', 'Jonah Lomu', 'Gareth Edwards', "Brian O'Driscoll"]
+
+player = input('What is your favourite rugby player? ')
+
+if player in favourite_rugby_players: 
+    print("Cool! He's my favourite player as well too! Let's be friends!")
+else: 
+    print('Oh...') 
