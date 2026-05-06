5d130a5acf229e746bd45b7dff3e5ef5018effed
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-05-06 09:38:44 +0000

    variable name change

--- Lists/TestingLists10.py
+++ Lists/TestingLists10.py
@@ -5,13 +5,13 @@ print('Words in the sentence: ', len(words))
 
 
 all_capitals = 0
-title_case = 0 
+title_capitals = 0 
 
 for word in words:
     if word.isupper():
         all_capitals += 1 
     elif word.istitle():
-        title_case += 1 
+        title_capitals += 1 
 
 print('All capital words:', all_capitals)
-print('Words starting with a capital letter:', title_case)
+print('Words starting with a capital letter:', title_capitals)
