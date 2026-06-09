03eb2113846c36477494b463aa3a8b349f06691a
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-06-09 21:24:12 +0000

    IDK

--- Dictionarys/TestingDictionarys.py
+++ Dictionarys/TestingDictionarys.py
@@ -9,9 +9,9 @@ while True:
     except ValueError:
         break
 
-    if run_time > 0:
-    run_times.append(run_time)
-    
+    if run_time > 0: 
+        run_times.append(run_time)
+        
     if run_time > best_time:
         best_time > run_time
 
