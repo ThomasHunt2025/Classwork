497fd94043ab4e63f8c885df8014493a24a5b47c
ThomasHunt2025 <th24074@students.sacredheart.school.nz>
2026-06-25 01:56:49 +0000

    change

--- Dictionarys/Dictionarystask2.py
+++ Dictionarys/Dictionarystask2.py
@@ -1,4 +1,9 @@
-def get_input():
+'''
+This program repeatedly asks the user to enter a car manufacturer
+'''
+
+
+def get_car():
     '''Repeatedly ask the user to enter the car manufacturer name'''
     key = input('Enter a car manufacturer (or press enter to finish): ').title()
     return key.strip()
@@ -7,16 +12,17 @@ car_dictionary = {}
 
 flag = True
 while flag:
-    key = get_input()
+    key = get_car()
     '''If the user presses enter, the code stops'''
+    '''Uses a dictionary to store how many times each car brand was entered'''
     if key == '':
         flag = False
-        
     elif key in car_dictionary:
         car_dictionary[key] += 1
     else:
         car_dictionary[key] = 1
 
+'''Prints a summary showing each car brand and how many times it was entered'''
 print('Summary of car manufacturers:')
 
 for key in sorted(car_dictionary):
